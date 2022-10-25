b54191621dac8334d4a077c899b23970d5c76c5c
unknown <vansang0105@gmail.com>
2022-10-25 18:48:18 +0700

    ud

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {WebhookClient} = require('dialogflow-fulfillment');
+var firebase = require('firebase')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCv49tXi0UC27uLzyUnqTV9c1bOWVWHrVE",
+    authDomain: "controldevices-f56de.firebaseapp.com",
+    projectId: "controldevices-f56de",
+    storageBucket: "controldevices-f56de.appspot.com",
+    messagingSenderId: "66509810211",
+    appId: "1:66509810211:web:264cf827d071b72ba19f96",
+    measurementId: "G-RLQBYK07SG"
+};
+
+firebase.initializeApp(firebaseConfig) 
+let database = firebase.database()
 
 const app = express()
 app.use(bodyParser.json())
@@ -22,12 +36,23 @@ const dialogflowFulfillment = (request, response) => {
     }
 
     function turnonled(agent){
+        let updates = {};
+        updates['devices/device1'] = true;
+        firebase.database().ref().update(updates);
         agent.add("Đã mở đèn");
+    }
+
+    function turnoffled(agent){
+        let updates = {};
+        updates['devices/device1'] = false;
+        firebase.database().ref().update(updates);
+        agent.add("Đã tắt đèn");
     }
 
     let intentMap = new Map();
     intentMap.set("Default Welcome Intent", sayHello);
     intentMap.set("turnonled", turnonled);
+    intentMap.set("turnoffled", turnoffled);
     agent.handleRequest(intentMap);
 
 }
